cb5d13ab52bc61fd00478d3b431c39a75a10ab00
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-05-30 12:03:17 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
-import { HomebridgeVirtualDevice } from './platform';
+import { VirtualDevice } from './platform';
 
 /**
  * This method registers the platform with Homebridge
  */
 export default (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, HomebridgeVirtualDevice);
+  api.registerPlatform(PLATFORM_NAME, VirtualDevice);
 };
