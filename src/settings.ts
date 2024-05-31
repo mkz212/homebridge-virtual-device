7f35f1be4681912e0c9b492293325ee54f65ae18
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-05-31 20:14:27 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeVirtualDevice';
+export const PLATFORM_NAME = 'VirtualDevice';
 
 /**
  * This must match the name of your plugin as defined the package.json
