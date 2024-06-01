afc98b12dfe382202f1d4950cf28210d31180026
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-06-01 08:36:01 +0200

    Update platformAccessory.ts

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -76,7 +76,7 @@ export class VirtualDeviceAccessory {
       this.motionSensor = this.accessory.getService('Motion Sensor One Name') ||
         this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
     } else {
-      const removeService = this.accessory.getService('Motion Sensor One Name);
+      const removeService = this.accessory.getService('Motion Sensor One Name');
       if (removeService) {
         this.accessory.removeService(removeService);
       }
