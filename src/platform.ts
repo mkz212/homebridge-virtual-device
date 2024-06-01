bc77e6ebc772067ac9874525da966634acb38089
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-06-01 11:44:35 +0200

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -71,7 +71,7 @@ export class VirtualDevicePlatform implements DynamicPlatformPlugin {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.name);
+      const uuid = this.api.hap.uuid.generate(`${device.name}+${device.type}`);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
